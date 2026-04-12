7289cbc65a7cf2eba5c275ab791cbb2593872441
Olamide <ayomikungbada@gmail.com>
2026-04-12 14:46:01 +0100

    Read config from environment variables

--- email_spam_classifier/settings.py
+++ email_spam_classifier/settings.py
@@ -1,4 +1,4 @@
-
+from decouple import config
 
 from pathlib import Path
 
@@ -10,13 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@gldv0y)c8y9q7rb@ua*6wjqumv=uyxawj6h_63#*j92a)k_g9'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 # Application definition
 
