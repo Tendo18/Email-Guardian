444e90789978495e9e694b780546e5312ba7af37
Olamide <ayomikungbada@gmail.com>
2026-04-12 15:43:28 +0100

    Fix startup crash when no model exists

--- classifier/apps.py
+++ classifier/apps.py
@@ -2,4 +2,15 @@ from django.apps import AppConfig
 
 
 class ClassifierConfig(AppConfig):
-    name = 'classifier'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "classifier"
+
+    def ready(self):
+        import sys
+        if "migrate" in sys.argv or "makemigrations" in sys.argv:
+            return
+        try:
+            from classifier.model_service import ModelService
+            ModelService.get_instance().load()
+        except Exception:
+            pass
