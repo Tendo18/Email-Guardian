a9d0044d13e84aab254aac5081b4eb9a5e2d3841
Olamide <ayomikungbada@gmail.com>
2026-05-13 16:26:54 +0100

    fix allowed hosts

--- email_spam_classifier/settings.py
+++ email_spam_classifier/settings.py
@@ -15,7 +15,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = [
+    'email-guardian-3hd9.onrender.com',
+    'localhost',
+    '127.0.0.1',
+    os.environ.get('ALLOWED_HOSTS', ''),
+]
 
 # Application definition
 
